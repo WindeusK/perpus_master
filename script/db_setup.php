8df61cbf09c9239d9ac7647ceb60c52d5a9b956e
WindeusK <129594654+WindeusK@users.noreply.github.com>
2023-05-06 18:12:51 +0700

    test

--- script/db_setup.php
+++ script/db_setup.php
@@ -1,7 +1,17 @@
 <?php
-    $status_json = file_get_contents("db_status.json");
+    $status_json = file_get_contents("./script/db_status.json");
     $status = json_decode($status_json);
-    if ($status->test) {
-        echo "Hello world!";
+    if ($status->complete == false) {
+        require('./script/conn.php');
+
+        $query = "CREATE DATABASE IF NOT EXISTS perpus_master";
+        if ($conn->query($query) == true) {
+            //Create Database success
+            $status->complete = true;
+            $status_json = json_encode($status);
+            file_put_contents('./script/db_status.json', $status_json);
+        } else {
+            echo "Error: " . $conn->error;
+        }
     }
 ?>
