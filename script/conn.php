dac7b105c6699b90d68c8dc133dcc59e6fc53c26
WindeusK <129594654+WindeusK@users.noreply.github.com>
2023-05-09 15:05:56 +0700

    reset

--- script/conn.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $server = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "perpus_master";
-
-    $conn = new mysqli($server, $username, $password);
-    $conn_db = new mysqli($server, $username, $password, $dbname)
-?>
--- script/settings.php
+++
@@ -1,4 +0,0 @@
-<?php
-    $settings_json = file_get_contents("./script/settings.json");
-    $settings = json_decode($settings_json);
-?>
