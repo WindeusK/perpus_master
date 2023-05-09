12b6f8af9bcaafd2f1e8783085e0a210f0b19974
WindeusK <129594654+WindeusK@users.noreply.github.com>
2023-05-09 15:54:03 +0700

    main page done

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" type="text/css" href="./style.css">
+
+    <title>Perpustakan SMKN1 Tanjungpandan</title>
+</head>
+<body>
+    <div class="container-fluid">
+        <div class="d-flex flex-column justify-content-center align-items-center vh-100">
+            <img src="https://smkn1tanjungpandan.sch.id/wp-content/uploads/2022/10/logo-smk-1-5.png" alt="Logo" class="img-fluid rounded mainimg">
+            <h1 class="mt-4">Selamat Datang di</h1>
+            <h2>Perpustakaan SMKN1 Tanjungpandan</h2>
+            <div class="mt-5">
+                <div class="mb-3">
+                    <a href="./buku.php" class="btn btn-outline-primary btn-lg d-block">Buku</a>
+                </div>
+                <div class="mb-3">
+                    <a href="./member.php" class="btn btn-outline-primary btn-lg d-block">Member</a>
+                </div>
+                <div class="mb-3">
+                    <a href="./peminjaman.php" class="btn btn-outline-primary btn-lg d-block">Peminjaman</a>
+                </div>
+                <div class="mb-5">
+                    <a href="./buat_database.php" class="btn btn-outline-primary btn-lg d-block">Buat Database</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
