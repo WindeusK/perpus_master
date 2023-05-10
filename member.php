eb3475bb66bdf58023b479f6d0c1a2e6da40e5c2
WindeusK <129594654+WindeusK@users.noreply.github.com>
2023-05-10 15:15:18 +0700

    member view done

--- member.php
+++ member.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Daftar Buku</title>
+    <title>Daftar Member</title>
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="./style.css">
@@ -18,7 +18,7 @@
             <a class="navbar-brand" href="index.php">Home</a>
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item">
-                    <a class="nav-link" href="tambahBuku.php">Tambah</a>
+                    <a class="nav-link" href="tambahMember.php">Tambah</a>
                 </li>
             </ul>
         </div>
@@ -26,15 +26,15 @@
 
     <!-- Content -->
     <div class="container">
-        <h1>Daftar Buku</h1>
+        <h1>Daftar Member</h1>
         <table class="table">
             <thead>
                 <tr>
                     <th>ID</th>
-                    <th>ISBN</th>
-                    <th>Judul</th>
-                    <th>Genre</th>
-                    <th>Description</th>
+                    <th>Nama</th>
+                    <th>Email</th>
+                    <th>No. Telepon</th>
+                    <th>Alamat</th>
                     <th>Actions</th>
                 </tr>
             </thead>
@@ -44,23 +44,23 @@
                 // Include the database connection file
                 require('./script/conn_db.php');
 
-                // Fetch data from the 'buku' table
-                $sql = "SELECT * FROM buku";
+                // Fetch data from the 'member' table
+                $sql = "SELECT * FROM member";
                 $result = $conn_db->query($sql);
 
                 // Loop through the rows and display the data
                 if ($result->num_rows > 0) {
                     while ($row = $result->fetch_assoc()) {
                         echo "<tr>";
-                        echo "<td>" . $row['id_buku'] . "</td>";
-                        echo "<td>" . $row['isbn'] . "</td>";
-                        echo "<td>" . $row['judul'] . "</td>";
-                        echo "<td>" . $row['genre'] . "</td>";
-                        echo "<td>" . $row['desc'] . "</td>";
+                        echo "<td>" . $row['id_member'] . "</td>";
+                        echo "<td>" . $row['nama'] . "</td>";
+                        echo "<td>" . $row['email'] . "</td>";
+                        echo "<td>" . $row['telepon'] . "</td>";
+                        echo "<td>" . $row['alamat'] . "</td>";
                         echo "<td>";
-                        echo "<a href='editBuku.php?id=" . $row['id_buku'] . "' class='btn btn-primary'>Edit</a>";
+                        echo "<a href='editMember.php?id=" . $row['id_member'] . "' class='btn btn-primary'>Edit</a>";
                         echo " ";
-                        echo "<a href='./script/deleteBuku.php?id=" . $row['id_buku'] . "' class='btn btn-danger'>Delete</a>";
+                        echo "<a href='./script/deleteMember.php?id=" . $row['id_member'] . "' class='btn btn-danger'>Delete</a>";
                         echo "</td>";
                         echo "</tr>";
                     }
@@ -72,7 +72,6 @@
         </table>
     </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
